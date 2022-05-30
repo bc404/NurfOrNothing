e2fd525a06f1ff48d5665f609dd4ec1b5b51f83f
northmac <98896973+northmac@users.noreply.github.com>
2022-05-30 14:20:46 -0400

    added gravity
    added gravity and isOnGround function

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -1,6 +1,8 @@
 /* Chandler, May 30 
 initialized the players character controller
 created method that receives inputs from input manager and applies them to the character controller
+added gravity 
+now detects if player is on ground
 */ 
 using System.Collections;
 using System.Collections.Generic;
@@ -10,7 +12,9 @@ public class PlayerMotor : MonoBehaviour
 {
     private CharacterController controller; 
     private Vector3 playerVelocity; 
+    private bool isGrounded; 
     public float speed = 5f; 
+    public float gravity = -9.8f; 
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +25,7 @@ public class PlayerMotor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        isGrounded = controller.isGrounded; 
     }
 
     //receive the inputs for our InputManager.cs and apply them to our character controller
@@ -33,6 +37,11 @@ public class PlayerMotor : MonoBehaviour
         //translates input y direction to movement across the z axis (forward and backward)
         moveDirection.z = input.y; 
         controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime); 
+        playerVelocity.y += gravity * Time.deltaTime; 
+        if(isGrounded && playerVelocity.y < 0 )
+              playerVelocity.y = -2f; 
+        controller.Move(playerVelocity * Time.deltaTime); 
+        Debug.Log(playerVelocity.y); 
     }
 
 }
