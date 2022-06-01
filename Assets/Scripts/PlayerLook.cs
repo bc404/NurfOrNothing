6e93771d6bdaae30318f1710345401b9e569ee28
northmac <canadachanmac@gmail.com>
2022-06-01 10:07:44 -0400

    added course library and cursor now disappears

--- Assets/Scripts/PlayerLook.cs
+++ Assets/Scripts/PlayerLook.cs
@@ -11,8 +11,14 @@ public class PlayerLook : MonoBehaviour
     private float xRotation = 0f; 
 
     public float xSensitivity = 30f; 
-    public float ySensitivity = 30f; 
-    
+    public float ySensitivity = 30f;
+
+    void Start()
+    {
+        //Set Cursor to not be visible
+        Cursor.visible = false;
+    }
+
     public void ProcessLook(Vector2 input)
     {
         float mouseX = input.x; 
