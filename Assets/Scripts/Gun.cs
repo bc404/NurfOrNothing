1d0a39cf88b4d81f5b599ad1d9e792fca4228ce8
northmac <98896973+northmac@users.noreply.github.com>
2022-06-02 14:12:47 -0400

    added gun effect
    smoke appears from damageable objects when they are shot

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -21,7 +21,7 @@ public class Gun : MonoBehaviour
         {
             if (hit.collider.GetComponent<Damageable>() != null)
             {
-                hit.collider.GetComponent<Damageable>().TakeDamage(damage); 
+                hit.collider.GetComponent<Damageable>().TakeDamage(damage, hit.point, hit.normal); 
             }
         }
     }
