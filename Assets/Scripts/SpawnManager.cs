0a8c7c0a29902df1181bc2ad640b66e99c96c9fb
northmac <98896973+northmac@users.noreply.github.com>
2022-06-07 13:33:32 -0400

    enemy spawns at random position

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    private float xRange = 48; 
+    private float minZRange = -8; 
+    private float maxZRange = 48; 
 
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, new Vector3(0, 1, 6),
-       enemyPrefab.transform.rotation); 
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation); 
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-xRange, xRange); 
+        float spawnPosZ = Random.Range(minZRange, maxZRange); 
+        Vector3 randomPos = new Vector3(spawnPosX, 1, spawnPosZ); 
+        return randomPos; 
     }
 
     // Update is called once per frame
