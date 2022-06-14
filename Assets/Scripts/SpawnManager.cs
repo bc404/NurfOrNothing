0943a9822d1fe663ac811d10b7012ef902292752
northmac <98896973+northmac@users.noreply.github.com>
2022-06-14 15:14:00 -0400

    new enemy prefab uses spawn manager script

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,52 +8,22 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject enemyPrefab;
-    private float xRange = 48; 
-    private float minZRange = -8; 
-    private float maxZRange = 48; 
-
-    public int enemyCount; 
-    public int waveNumber = 1;
+    public GameObject enemyPrefab; 
 
     
 
     // Start is called before the first frame update
     void Start()
     {
-        SpawnEnemyWave(waveNumber); 
+        Instantiate(enemyPrefab, new Vector3(0, 2, 6), enemyPrefab.transform.rotation); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        enemyCount = FindObjectsOfType<Enemy>().Length; 
-        if (enemyCount == 0)
-        {
-            waveNumber++; 
-            SpawnEnemyWave(waveNumber); 
-        }
+       
     }
 
-    private Vector3 GenerateSpawnPosition()
-    {
-        float spawnPosX = Random.Range(-xRange, xRange); 
-        float spawnPosZ = Random.Range(minZRange, maxZRange); 
-        Vector3 randomPos = new Vector3(spawnPosX, 3, spawnPosZ); 
-        return randomPos; 
-    }
-
-    void SpawnEnemyWave(int enemiesToSpawn)
-    {
-        for (int i = 0; i < enemiesToSpawn; i++)
-        {
-            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation); 
-        }
-
-        
-    }
-
-    
-
-    
+  
+   
 }
